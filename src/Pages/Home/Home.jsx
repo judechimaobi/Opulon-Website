87a39a2b76fb02496468c2978b12248ffa14e500
bello hadi <bellohadi82@gmail.com>
2024-08-28 13:03:17 +0100

    :sparkles: finished the opulon section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,17 +1,69 @@
+import { IoPlayCircleOutline } from "react-icons/io5";
+import * as images from "../../images"
 const Home = ()=>{
     return(
         <>
-            <div className="flex px-20">
-                <div className="flex">
+            <div className="flex flex-col px-20">
+                {/* banner start */}
+                <div className="flex items-center">
                     <div className="flex flex-col">
-                        <p className="text-transparent bg-clip-text bg-gradient-to-r from-[#FFFFFF] to-[#702A90] w-[500px] font-normal text-5xl">
+                        <p className="font-sans1 text-transparent bg-clip-text bg-gradient-to-r from-[#FFFFFF] to-[#702A90] w-[650px] font-normal text-5xl">
                             Welcome to Opulon:
                             An Immersive Virtual
                             World Built on Solana
                         </p>
+                        <p className="mt-3 mb-8 font-sans2 text-[#FB4C00] text-lg w-[550px]">
+                            Trade, Play, and Connect in a Seamless Crypto Ecosystem
+                        </p>
+                        <div className="flex font-sans2 text-[#fff] w-[500px] items-center">
+                            <p className="flex justify-center  font-sans2 bg-[#670D65] rounded-full p-5">
+                                Join the Adventure
+                            </p>
+                            <p className="flex ml-3"><IoPlayCircleOutline />&nbsp;see Demo</p>
+                        </div>
                     </div>
-                    <div></div>
+                    <div className="bg-[url('/src/assets/output.png')] w-[500px] h-[703px] bg-center bg-cover">
+                        
+                    </div>
                 </div>
+                {/* banner ends */}
+                {/* what is opulon start */}
+                <div className="flex justify-between my-10">
+                    <div className="flex w-1/2">
+                        <img src={images.Frame55} />
+                        <div className="flex items-center flex-col ml-5">
+                            <img src={images.Frame56} />
+                            <img className="mt-5" src={images.Frame57} />
+                        </div>
+                    </div>
+                    <div className="flex flex-col w-1/2">
+                        <p className="font-sans1 text-transparent bg-clip-text bg-gradient-to-r from-[#E99A4E] to-[#FFFFFF] w-[650px] font-normal text-2xl">
+                            What is Opulon?
+                        </p>
+                        <p className="text-base font-sans2 text-[#fff] w-[600px] mt-2 mb-9">
+                            Opulon is a virtual crypto world built on the Solana blockchain, designed to 
+                            bring blockchain activities like trading, gaming, and social interaction into 
+                            a unified and immersive environment.
+                        </p>
+                        <div className="flex flex-col">
+                            <p className="mb-4 font-sans1 text-transparent bg-clip-text bg-gradient-to-r from-[#E99A4E] to-[#FFFFFF] w-[650px] font-normal text-2xl">
+                                Key Benefits
+                            </p>
+                            <div className="flex font-sans2 text-[#fff]">
+                                <p className="flex justify-center text-center font-sans2 rounded-md border-2 border-[#702A90] p-5">
+                                    Seamless integration of blockchain activities
+                                </p>
+                                <p className="flex justify-center mx-5 text-center font-sans2 rounded-md border-2 border-[#702A90] p-5">
+                                    Immersive, interactive virtual world
+                                </p>
+                                <p className="flex justify-center text-center font-sans2 rounded-md border-2 border-[#702A90] p-5">
+                                    Secure and fast transactions with Solana
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* what is opulon ends */}
             </div>
         </>
     )
--- src/images.js
+++ src/images.js
@@ -1,7 +1,13 @@
 import opulon from "./assets/opulon.png";
-import output from "./assets/output.png"
+import output from "./assets/output.png";
+import Frame55 from "./assets/Frame55.png";
+import Frame56 from "./assets/Frame56.png";
+import Frame57 from "./assets/Frame57.png";
 
 export {
     opulon,
-    output
+    output,
+    Frame55,
+    Frame56,
+    Frame57
 }
